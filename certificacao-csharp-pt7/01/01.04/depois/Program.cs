31bcd9ab33fdab028204d6296aaa9abec42f09a4
Henrique Santos <henrique.144@outlook.com>
2023-01-23 10:16:52 -0300

    Func, Action e Predicate

--- certificacao-csharp-pt7/01/01.04/depois/Program.cs
+++ certificacao-csharp-pt7/01/01.04/depois/Program.cs
@@ -11,11 +11,13 @@ namespace _01_04
             Operacao operacao = (x, y) => x + y; //expressão lambda
             Console.WriteLine(operacao(3, 2));
 
+            // Delegado de Função (possui retorno)
             Func<int, int, int> somar = (x, y) => x + y;
             Console.WriteLine(somar(3, 2));
 
-            Action<string> imprimirMensagem 
-                = (mensagem) => 
+            // Delegado de Ação (não possui retorno)
+            Action<string> imprimirMensagem = 
+                (mensagem) => 
                 {
                     Console.WriteLine(mensagem);
                 };
@@ -24,7 +26,9 @@ namespace _01_04
 
             var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Console.WriteLine("Números divisíveis por 3:");
+            // Predicate é um tipo de Delegado que por padrão retorna um valor booleano
             Predicate<int> divisivelPor3 = (numero) => numero % 3 == 0;
+            // Retorna uma nova lista, com todos os números que atendem ao Predicate(divisivelPor3)
             var divisiveis = Array.FindAll(numeros, divisivelPor3);
 
             foreach (var numero in divisiveis)
